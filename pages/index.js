af8dad468674960319d09c66f225db9eae494c87
Himanshu Srivastava <iamhimanshusrivastava@gmail.com>
2021-10-11 16:40:12 +0530

    Updated the header

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 
 import Avatar from '../components/Avatar';
+import { MicrophoneIcon, ViewGridIcon } from "@heroicons/react/solid";
 
 export default function Home() {
   return (
@@ -22,6 +23,8 @@ export default function Home() {
         <div className="flex space-x-4 items-center">
           <p className="link">Gmail</p>
           <p className="link">Images</p>
+
+          <ViewGridIcon className="h-10 w-10 p-2 rounded-full hover:bg-gray-100 cursor-pointer" />
 
           <Avatar url="Avatar.png" />
         </div>
