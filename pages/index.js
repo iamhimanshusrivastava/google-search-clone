4580ea4d54aa3fca5af9bbc325d92e202d48a24a
Himanshu Srivastava <iamhimanshusrivastava@gmail.com>
2021-10-16 15:03:59 +0530

    building the search functionality

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
+import { useRef } from 'react';
+import { useRouter } from "next/router";
 
 import Avatar from '../components/Avatar';
 import { MicrophoneIcon, SearchCircleIcon, ViewGridIcon } from "@heroicons/react/solid";
@@ -8,6 +10,18 @@ import { SearchIcon } from "@heroicons/react/outline";
 import Footer from "../components/Footer";
 
 export default function Home() {
+  const router = useRouter();
+  const searchInputRef = useRef(null);
+
+  const search = (event) => {
+    event.preventDefault();
+    const term = searchInputRef.current.value;
+
+    if (!term) return;
+
+    router.push(`/search?term=${term}`);
+  }
+
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <Head>
@@ -38,13 +52,17 @@ export default function Home() {
         <Image src="https://www.google.co.uk/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" height={100} width={300} />
         <div className="flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl">
           <SearchIcon className="h-5 mr-3 text-gray-500" />
-          <input type="text" className="flex-grow focus:outline-none" />
+          <input
+            ref={searchInputRef}
+            type="text"
+            className="flex-grow focus:outline-none"
+          />
           <MicrophoneIcon className="h-5" />
         </div>
 
         <div className="flex flex-col w-1/2 space-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm:space-x-4">
-          <button className="btn">Google Search</button>
-          <button className="btn">I'm feeling Lucky</button>
+          <button onClick={search} className="btn">Google Search</button>
+          <button onClick={search} className="btn">I'm feeling Lucky</button>
         </div>
       </form>
 
