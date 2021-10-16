d34b00fb85bc3a6c5daa16638759fec31576f5db
Himanshu Srivastava <iamhimanshusrivastava@gmail.com>
2021-10-16 15:21:00 +0530

    Initial commit - building Search Results Page

---
+++ pages/search.js
@@ -0,0 +1,19 @@
+import Head from 'next/head';
+
+function Search() {
+    return (
+        <div>
+            <Head>
+                <title>Search Results</title>
+                <link rel='icon' href="/favicon.ico" />
+            </Head>
+
+            {/* {Header} */}
+
+            {/* {Search Results}  */}
+
+        </div>
+    )
+}
+
+export default Search
